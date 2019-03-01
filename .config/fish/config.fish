92712ddd190c48ff41d78673473bdcdac146b240
Delon Newman <delnewman@salud.unm.edu>
2019-03-01 13:18:05 -0700

    updated .gitignore, added .minttyrc, and move ~/.rvm/bin to back of PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,2 @@
-set PATH $PATH $HOME/bin $HOME/.rvm/bin /cygdrive/c/Users/delnewman/local/instantclient_12_2
+set PATH $PATH $HOME/bin /cygdrive/c/Users/delnewman/local/instantclient_12_2 $HOME/.rvm/bin
 rvm default
